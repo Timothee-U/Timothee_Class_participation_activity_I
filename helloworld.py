a955efa3f56fdfdb8cbba5053ae0881fb7650423
Timothee-U <t.uwayesualustudent.com>
2025-02-26 01:35:52 -0800

    Task 1: Hello world

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# helloworld.py
+# This is a Python script to print a greeting and use variables
+
+# Printing Hello World
+print("Hello, World!")
+
+# Asking and storing user's name
+name = input("Please enter your name: ")
+
+#Greeting using the user's name
+print(f"Hello, {name}!")
