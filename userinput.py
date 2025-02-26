9d37b1b64eaef039c894be46e7f5978876164afc
Timothee-U <t.uwayesualustudent.com>
2025-02-26 01:39:55 -0800

    Task 3: user input

---
+++ userinput.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# This is a Python script that takes 2 user inputed numbers, adds two numbers, and displays the result
+
+# Here we are sking the user to input two numbers
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Making sure the inputs are strictly integers
+num1 = int(num1)
+num2 = int(num2)
+
+# Adding the two numbers together
+result = num1 + num2
+
+# Displaying the result
+print(f"The sum of {num1} and {num2} is {result}.")
